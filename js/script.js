642b15235178faf81aea810ed6d22d108b2f5011
Deivid Galindo <deivid.dd223@gmail.com>
2025-03-26 20:34:55 -0300

    Adicionado o metodo de soma

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-// start nos btns
-let buttons = document.querySelectorAll(".btn");
+class Calculator{
 
-console.log(buttons);
+    constructor(){
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
+        this.reset = 0;
+    }
+
+    // metodo de soma
+    sum(n1, n2){
+        return parseFloat(n1) + parseFloat(n2)
+
+    }
+
+    resolution(){
+        // nos jogamos para um array uma string
+        let upperValueArray = (this.upperValue.textContent).split(" ");
+        result = calc.sum(upperValueArray[i - 1], upperValueArray[i + 1]);
+    }
+}
+
+// start nos btns
+let buttons = document.querySelectorAll('.btn');
+
+console.log('teste');
