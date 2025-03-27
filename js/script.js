3d4ae66ee7b8e57701cc7bb57f0502c764de3b3e
Deivid Galindo <deivid.dd223@gmail.com>
2025-03-26 21:48:17 -0300

    Incluido metodo de soma, subtração, divisão e multiplicação, na divisão inclui mensagem de erro caso digitem zero

--- js/script.js
+++ js/script.js
@@ -8,18 +8,52 @@ class Calculator{
 
     // metodo de soma
     sum(n1, n2){
-        return parseFloat(n1) + parseFloat(n2)
-
+        return n1 + n2;
     }
 
-    resolution(){
-        // nos jogamos para um array uma string
-        let upperValueArray = (this.upperValue.textContent).split(" ");
-        result = calc.sum(upperValueArray[i - 1], upperValueArray[i + 1]);
+    // metodo subtração
+    subtraction(n1, n2){
+        return n1 - n2;
     }
+    // metodo divisão
+    division(n1, n2){
+        try { 
+            if (n2===0){
+                throw new Error('Divisão por zero');
+            }
+            return n1 / n2;
+        } catch(error) {
+            return `Erro: ${error.message}`;
+        }
+    }
+    // metodo multiplicação
+    multiplication(n1, n2){
+        return n1 * n2;
+    }
+
 }
+
+// criando objeto
+const calc = new Calculator;
 
 // start nos btns
 let buttons = document.querySelectorAll('.btn');
 
 console.log('teste');
+
+// chamado metodo soma
+let resultado = calc.sum(1,2);
+console.log(resultado);
+
+// chamado metodo subtração
+resultado = calc.subtraction(2,1);
+console.log(resultado)
+
+// chamado metodo divisão
+resultado = calc.division(4,2);
+console.log(resultado);
+
+// chamado metodo multiplicação
+resultado = calc.multiplication(2,2)
+console.log(resultado)
+
