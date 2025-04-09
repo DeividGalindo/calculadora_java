5215b3235ef2c4bd2f2147395bf07a44d6e7cad7
Deivid Galindo <deivid.dd223@gmail.com>
2025-04-09 19:37:00 -0300

    Adicionado funcionalidade ao botão AC

--- js/script.js
+++ js/script.js
@@ -1,79 +1,66 @@
 class Calculator{
-
     constructor(){
         this.upperValue = document.querySelector('#upper-number');
         this.resultValue = document.querySelector('#result-number');
         this.reset = 0;
     }
+        //metodo de adição
+        sum(n1, n2){
+            return n1+n2;
+        }
+        //metodo de subtração
+        sub(n1, n2){
+            return n1-n2;
+        }
+        //metodo de divisão
+        division(n1, n2){
+            return n1/n2;
+        }
 
-    //método soma
-    sum(n1, n2){
-        return n1 + n2;
-    }
+        multiplication(n1, n2){
+            return n1*n2;
+        }
 
-    subtraction(n1, n2){
-        return n1 - n2;
-    }
+        clearValue(){
+            this.upperValue.textContent = '0';
+            this.resultValue.textContent = '0';
+            this.reset = false;
+        }
 
-    divide(n1, n2) {
-        try {
-            if (n2 === 0){
-                throw new Error('Divisão por zero');
+        btnPress = (event) =>{
+            const input = event.target.textContent;
+            let currentExpression = this.upperValue.textContent;
+
+            if(input === 'AC'){
+                this.clearValue();
+                return;
             }
-            return n1 / n2;
-        } catch (error) {
-            return Erro; {error.message};
         }
-    }
 
-    multiply(n1, n2){
-        return n1 * n2;
-    }
-
-    //Começar aqui
-    clearValues(){
-        this.upperValue.textContent = '0';
-        this.resultValue.textContent = '0';
-        this.reset = false; 
-    }
-
-    btnPress = (event) => {
-        const input = event.target.textContent; 
-        let currentExpression = this.upperValue.textContent;
-
-        //Limpa
-        if (input === 'AC') {
-            this.clearValues(); //Limpa o valor da calculadora
-            return;
-        }
-    }
-    
 }
 
 
 //criando o objeto
-const calc = new Calculator();
-document.querySelector('#upper-number').textContent = '5 X 6 =';
+const calc = new Calculator;
+document.querySelector('#upper-number').textContent = '5 x 6 = ';
 
 //start nos btns
 let buttons = document.querySelectorAll('.btn');
-console.log('teste');
+// 
+// console.log('teste');
 
-const resultado= calc.sum(1,2);
-console.log(resultado);
+// let resultado = calc.sum(1,2);
+// console.log(resultado);
 
-//Chamando o método subitração
-let teste = calc.subtraction(5,4);
-console.log(teste);
+// resultado = calc.sub(5,4);
+// console.log(resultado);
 
-//Chamando o método divisão
-teste = calc.divide(10,5);
-console.log(teste);
+// resultado = calc.division(10,2);
+// console.log(resultado);
 
-//Chamando o método multiplicar
-teste = calc.multiply(5,4);
-console.log(teste);
-
-for (let i=0; buttons.length > i; i++){
-    buttons[i].addEventListener('click',calc.btnPress);
+// resultado = calc.multiplication(4,2);
+// console.log(resultado);
+// 
+for(let i = 0; buttons.length > i; i++){
+    buttons[i].addEventListener('click', calc.btnPress);
 }
