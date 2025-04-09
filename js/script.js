907410706ec4066be359342099f9c115df26f5ff
Deivid Galindo <deivid.dd223@gmail.com>
2025-04-09 19:35:59 -0300

    Adicionado funcionalidade ao botão AC

--- js/script.js
+++ js/script.js
@@ -6,58 +6,74 @@ class Calculator{
         this.reset = 0;
     }
 
-    // metodo de soma
+    //método soma
     sum(n1, n2){
         return n1 + n2;
     }
 
-    // metodo subtração
     subtraction(n1, n2){
         return n1 - n2;
     }
-    // metodo divisão
-    division(n1, n2){
-        try { 
-            if (n2===0){
+
+    divide(n1, n2) {
+        try {
+            if (n2 === 0){
                 throw new Error('Divisão por zero');
             }
             return n1 / n2;
-        } catch(error) {
-            return `Erro: ${error.message}`;
+        } catch (error) {
+            return Erro; {error.message};
         }
     }
-    // metodo multiplicação
-    multiplication(n1, n2){
+
+    multiply(n1, n2){
         return n1 * n2;
     }
 
+    //Começar aqui
+    clearValues(){
+        this.upperValue.textContent = '0';
+        this.resultValue.textContent = '0';
+        this.reset = false; 
+    }
+
+    btnPress = (event) => {
+        const input = event.target.textContent; 
+        let currentExpression = this.upperValue.textContent;
+
+        //Limpa
+        if (input === 'AC') {
+            this.clearValues(); //Limpa o valor da calculadora
+            return;
+        }
+    }
+    
 }
 
-// criando objeto
-const calc = new Calculator;
 
-// start nos btns
+//criando o objeto
+const calc = new Calculator();
+document.querySelector('#upper-number').textContent = '5 X 6 =';
+
+//start nos btns
 let buttons = document.querySelectorAll('.btn');
-
 console.log('teste');
 
-// chamado metodo soma
-let resultado = calc.sum(1,2);
+const resultado= calc.sum(1,2);
 console.log(resultado);
 
-// chamado metodo subtração
-resultado = calc.subtraction(2,1);
-console.log(resultado)
+//Chamando o método subitração
+let teste = calc.subtraction(5,4);
+console.log(teste);
 
-// chamado metodo divisão
-resultado = calc.division(4,2);
-console.log(resultado);
+//Chamando o método divisão
+teste = calc.divide(10,5);
+console.log(teste);
 
-// chamado metodo multiplicação
-resultado = calc.multiplication(2,2)
-console.log(resultado)
+//Chamando o método multiplicar
+teste = calc.multiply(5,4);
+console.log(teste);
 
 for (let i=0; buttons.length > i; i++){
     buttons[i].addEventListener('click',calc.btnPress);
 }
-
